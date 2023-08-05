8f2b617b7854eb0b659df0cc515de9ef938c6d12
Jose Manuel Hita Garcia <josemanuelhita992@hotmail.com>
2023-08-05 20:56:21 +0100

    working on the index page infinite scroll for phone size

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -9,55 +9,33 @@ const upload = multer({ storage });
 
 const Campground = require('../model/campground');
 
-router.get('/', catchAsync(async (req, res) => {
-  const perPage = 10; // Number of campgrounds per page
-  const page = Math.max(req.query.page ? parseInt(req.query.page) : 1, 1); //Get the page number from query parameters and Ensure the page is at least 1
-  const isPhoneSize = req.query.isPhoneSize === 'true';
-  const isJson = req.query.isJson === 'true'; // Add this line
+router.route('/')
+  .get(catchAsync(async (req, res) => {
+    const page = Math.max(req.query.page ? parseInt(req.query.page) : 1, 1); // Get page number from query parameters
+    const limit = 10; // Number of campgrounds per page
+    const skip = (page - 1) * limit; // Calculate the number of results to skip
 
-  // Check if the skip value is negative and redirect to the first page if so
-  const skipValue = (perPage * page) - perPage;
-  if (skipValue < 0) {
-    return res.redirect('/?page=1'); // Redirect to the first page if skip is negative
-  }
-
-
-
-  try {
     // Fetch paginated campgrounds from the database
-    const paginatedCampgrounds = await Campground.find({})
-    .skip((perPage * page) - perPage)
-    .limit(perPage);
+    const campgrounds = await Campground.find().skip(skip).limit(limit);
 
-    const allCampgrounds = await Campground.find({}); // Fetch all campgrounds for the cluster map
+    // Fetch all campgrounds for the cluster map
+    const allCampgrounds = await Campground.find();
 
     // Count all campgrounds to calculate total pages
-    const campgroundsCount = await Campground.countDocuments();
-    const totalPages = Math.ceil(campgroundsCount / perPage);
+    const totalCampgrounds = await Campground.countDocuments();
+    const totalPages = Math.ceil(totalCampgrounds / limit);
 
-    // Check if the response should be in JSON format
-    if (isJson) {
-      return res.json({ campgrounds: paginatedCampgrounds, totalPages }); // Return JSON response
+    // If it's a phone size, and isJson is true, return JSON response
+    const isPhoneSize = req.query.isPhoneSize === 'true';
+    const isJson = req.query.isJson === 'true';
+    if (isPhoneSize && isJson) {
+      return res.json({ campgrounds, totalPages });
     }
 
-    res.render('campgrounds/index', {
-      campgrounds: paginatedCampgrounds,
-      allCampgrounds,
-      currentPage: page,
-      totalPages,
-      isPhoneSize
-    });
-  } catch (err) {
-    console.error('Error fetching paginated campgrounds:', err);
-    // Handle the error appropriately, e.g., render an error page.
-    res.render('error', { err });
-  }
-
-}))
-
-
-router.post('/', isLoggedIn, upload.array('image'), validateCampground, catchAsync(campgrounds.createCampground))
-
+    // Render the index page with paginated campgrounds
+    res.render('campgrounds/index', { campgrounds, allCampgrounds, currentPage: page, totalPages, isPhoneSize });
+  }))
+  .post(isLoggedIn, upload.array('image'), validateCampground, catchAsync(campgrounds.createCampground));
 
 router.get('/new', isLoggedIn, campgrounds.renderNewForm)
 
